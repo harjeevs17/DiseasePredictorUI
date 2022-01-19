18c0dfbfa98bb2e2ff1cb697a3685bf3a8611d15
Singh <Harjeev.Singh@cerner.com>
2022-01-20 00:53:44 +0530

    Made changes

--- src/dataset/data.js
+++ src/dataset/data.js
@@ -1,12 +1,90 @@
 export const Symptoms = {
-    'eyes':['irritation','blurred','blindness'],
+    'eyes':['irritation','blurred_and_distorted_vision','blindness'],
     'chest':['headache','anixiety','nausea'],
     'upper_arm':['headache11','anixiety22','nausea33'],
     'nose':['continuous_sneezing','congestion'],
     'knee':['joint_pain','swelling_joints','knee_pain'],
     'mid_abdomen':['stomach_pain', 'constipation', 'distention_of_abdomen', 'increased_appetite','excessive_hunger'],
     'thigh':['hip_joint_pain'],
-    
-
-
 };
+
+export const DiseaseDetails = {
+    "Fungal Infection" : "A fungus that invades the tissue can cause a disease that\'s confined to the skin, spreads into tissue, bones and organs or affects the whole body.Symptoms depend on the area affected, but can include skin rash or vaginal infection resulting in abnormal discharge.Treatments include antifungal medication.",
+    "Allergy":"Allergies are your body's reaction to a normally harmless substance such as pollen, molds, animal dander, latex, certain foods and insect stings. Allergy symptoms range from mild – rash or hives, itchiness, runny nose, watery/red eyes – to life-threatening.",
+    "GERD":"A digestive disease in which stomach acid or bile irritates the food pipe lining.\
+    This is a chronic disease that occurs when stomach acid or bile flows into the food pipe and irritates the lining. Acid reflux and heartburn more than twice a week may indicate GERD.\
+    Symptoms include burning pain in the chest that usually occurs after eating and worsens when lying down.\
+    Relief from lifestyle changes and over-the-counter medication is usually temporary. Stronger medication may be required.",
+    "Chronic cholestasis":"Chronic cholestatic diseases, whether occurring in infancy, childhood or adulthood, are characterized by defective bile acid transport from the liver to the intestine, which is caused by primary damage to the biliary epithelium in most cases.",
+    "Drug Reaction":"A drug allergy is the abnormal reaction of your immune system to a medication. Any medication — over-the-counter, prescription or herbal — is capable of inducing a drug allergy. However, a drug allergy is more likely with certain medications. The most common signs and symptoms of drug allergy are hives, rash or fever.",
+    "Peptic ulcer diseae":"A peptic ulcer is a sore on the lining of your stomach, small intestine or esophagus. A peptic ulcer in the stomach is called a gastric ulcer. A duodenal ulcer is a peptic ulcer that develops in the first part of the small intestine (duodenum).",
+    "AIDS":"HIV causes AIDS and interferes with the body's ability to fight infections.\
+    The virus can be transmitted through contact with infected blood, semen or vaginal fluids.\
+    Within a few weeks of HIV infection, flu-like symptoms such as fever, sore throat and fatigue can occur. Then the disease is usually asymptomatic until it progresses to AIDS. AIDS symptoms include weight loss, fever or night sweats, fatigue and recurrent infections.\
+    No cure exists for AIDS, but strict adherence to antiretroviral regimens (ARVs) can dramatically slow the disease's progress as well as prevent secondary infections and complications.",
+    "Diabetes":"Diabetes is a number of diseases that involve problems with the hormone insulin. Normally, the pancreas (an organ behind the stomach) releases insulin to help your body store and use the sugar and fat from the food you eat. Diabetes can occur when the pancreas produces very little or no insulin, or when the body does not respond appropriately to insulin. As yet, there is no cure. People with diabetes need to manage their disease to stay healthy.",
+    "Gastroenteritis":"When you have diarrhea and vomiting, you may say you have the stomach flu. These symptoms often are due to a condition called gastroenteritis. With gastroenteritis, your stomach and intestines are irritated and inflamed. The cause is typically a viral or bacterial infection.",
+    "Bronchial Asthma":"Ever hear the term bronchial asthma and wonder what it means? When people talk about bronchial asthma, they are really talking about asthma, a chronic inflammatory disease of the airways that causes periodic attacks of coughing, wheezing, shortness of breath, and chest tightness.",
+    "Hypertension":"High blood pressure or hypertension increases the risk of heart disease and stroke. Hypertension risk factors include obesity, drinking too much alcohol, smoking, and family history. Beta-blockers are a common treatment for hypertension.", 
+    "Migraine":"A migraine is a strong headache that often comes with nausea, vomiting, and sensitivity to light. It can last hours or days.",
+    "Cervical spondylosis":"Cervical spondylosis is a general term for age-related wear and tear affecting the spinal disks in your neck. As the disks dehydrate and shrink, signs of osteoarthritis develop, including bony projections along the edges of bones (bone spurs).Cervical spondylosis is very common and worsens with age. More than 85 percent of people older than age 60 are affected by cervical spondylosis.Most people experience no symptoms from these problems. When symptoms do occur, nonsurgical treatments often are effective",
+    "Paralysis (brain hemorrhage)":"Paralysis is the loss of the ability to move some or all of your body. It can have lots of different causes, some of which can be serious. Depending on the cause, it may be temporary or permanent.",
+    "Jaundice":"Many newborn babies develop jaundice, a condition in which the skin and whites of the eyes are yellowish in color, within a few days after birth. The jaundice is caused by elevated levels of bilirubin, a substance made from the breakdown of old red blood cells that is normally removed by the liver.",
+    "Malaria":"Malaria is a disease caused by a parasite. The parasite is spread to humans through the bites of infected mosquitoes. People who have malaria usually feel very sick with a high fever and shaking chills. While the disease is uncommon in temperate climates, malaria is still common in tropical and subtropical countries.",
+    "Chicken pox":"Chickenpox is a highly contagious disease caused by the varicella-zoster virus (VZV). It can cause an itchy, blister-like rash. The rash first appears on the chest, back, and face, and then spreads over the entire body, causing between 250 and 500 itchy blisters.",
+    "Dengue":"Dengue is a viral infection transmitted to humans through the bite of infected mosquitoes. The primary vectors that transmit the disease are Aedes aegypti mosquitoes and, to a lesser extent, Ae. albopictus. The virus responsible for causing dengue, is called dengue virus (DENV).",
+    "Typhoid":"Typhoid is a bacterial infection that can lead to a high fever, diarrhea, and vomiting. It can be fatal. It is caused by the bacteria Salmonella typhi. The infection is often passed on through contaminated food and drinking water, and it is more prevalent in places where handwashing is less frequent.",
+    "hepatitis A":"Hepatitis A is preventable by vaccine. It spreads from contaminated food or water or contact with someone who is infected.\
+    Symptoms include fatigue, nausea, abdominal pain, loss of appetite and low-grade fever.\
+    The condition clears up on its own in one or two months. Rest and adequate hydration can help.",
+    "Hepatitis B":"Hepatitis B is a vaccine-preventable liver infection caused by the hepatitis B virus (HBV). Hepatitis B is spread when blood, semen, or other body fluids from a person infected with the virus enters the body of someone who is not infected.",
+    "Hepatitis C":"The virus is spread by contact with contaminated blood; for example, from sharing needles or from unsterile tattoo equipment.\
+    Most people have no symptoms. Those who do develop symptoms may have fatigue, nausea, loss of appetite and yellowing of the eyes and skin.\
+    Hepatitis C is treated with antiviral medication. In some people, newer medicines can eradicate the virus.",
+    "Hepatitis D":"Hepatitis D, also known as the hepatitis delta virus, is an infection that causes the liver to become inflamed. This swelling can impair liver function and cause long-term liver problems, including liver scarring and cancer. The condition is caused by the hepatitis D virus (HDV).",
+    "Hepatitis E":"Hepatitis E is an inflammation of the liver caused by infection with the hepatitis E virus (HEV). Every year there are an estimated 20 million HEV infections worldwide, leading to an estimated 3.3 million symptomatic cases of hepatitis E.",
+    "Alcoholic hepatitis":"Alcoholic hepatitis can occur in people who drink heavily for many years.\
+    Symptoms include yellow skin and eyes along with increasing stomach size due to fluid accumulation.\
+    Treatment involves hydration, nutritional care and stopping alcohol use. Steroid drugs can help reduce liver inflammation.",
+    "Tuberculosis":"The bacteria that cause TB are spread when an infected person coughs or sneezes.\
+    Most people infected with the bacteria that cause tuberculosis don't have symptoms. When symptoms do occur, they usually include a cough (sometimes blood-tinged), weight loss, night sweats and fever.\
+    Treatment isn't always required for those without symptoms. Patients with active symptoms will require a long course of treatment involving multiple antibiotics.",
+    "Common Cold":"In contrast to the flu, a common cold can be caused by many different types of viruses. The condition is generally harmless and symptoms usually resolve within two weeks.\
+    Symptoms include a runny nose, sneezing and congestion. High fever or severe symptoms are reasons to see a doctor, especially in children.\
+    Most people recover on their own within two weeks. Over-the-counter products and home remedies can help control symptoms.",
+    "Pneumonia":"Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia.",
+    "Dimorphic hemmorhoids(piles)":"Hemorrhoids, or piles, are a common problem. These swollen veins inside the rectum or outside the anus can cause pain, anal itching and rectal bleeding. Symptoms often improve with at-home treatments but on occasion people need medical procedures. Eating more fiber can help prevent hemorrhoids.",
+    "Heart attack":"A heart attack is a medical emergency. A heart attack usually occurs when a blood clot blocks blood flow to the heart. Without blood, tissue loses oxygen and dies.\
+    Symptoms include tightness or pain in the chest, neck, back or arms, as well as fatigue, lightheadedness, abnormal heartbeat and anxiety. Women are more likely to have atypical symptoms than men.\
+    Treatment ranges from lifestyle changes and cardiac rehabilitation to medication, stents and bypass surgery.",
+    "Varicose veins":"Varicose veins are generally benign. The cause of this condition is not known.\
+    For many people, there are no symptoms and varicose veins are simply a cosmetic concern. In some cases, they cause aching pain and discomfort or signal an underlying circulatory problem.\
+    Treatment involves compression stockings, exercise or procedures to close or remove the veins.",
+    "Hypothyroidism":"Hypothyroidism's deficiency of thyroid hormones can disrupt such things as heart rate, body temperature and all aspects of metabolism. Hypothyroidism is most prevalent in older women.\
+    Major symptoms include fatigue, cold sensitivity, constipation, dry skin and unexplained weight gain.\
+    Treatment consists of thyroid hormone replacement.",
+    "Hyperthyroidism":"Hyperthyroidism is the production of too much thyroxine hormone. It can increase metabolism.\
+    Symptoms include unexpected weight loss, rapid or irregular heartbeat, sweating and irritability, although the elderly often experience no symptoms.\
+    Treatments include radioactive iodine, medication and sometimes surgery.",
+    "Hypoglycemia":"Hypoglycemia is a condition in which your blood sugar (glucose) level is lower than normal. Glucose is your body's main energy source. Hypoglycemia is often related to diabetes treatment. But other drugs and a variety of conditions — many rare — can cause low blood sugar in people who don't have diabetes.",
+    "Osteoarthristis":"Osteoarthritis is the most common form of arthritis, affecting millions of people worldwide. It occurs when the protective cartilage that cushions the ends of the bones wears down over time. Although osteoarthritis can damage any joint, the disorder most commonly affects joints in your hands, knees, hips and spine.",
+    "Arthritis":"Arthritis is the swelling and tenderness of one or more joints. The main symptoms of arthritis are joint pain and stiffness, which typically worsen with age. The most common types of arthritis are osteoarthritis and rheumatoid arthritis.",
+    "(vertigo) Paroymsal  Positional Vertigo":"Paroxysmal positional vertigo (BPPV) is triggered by certain changes in head position, such as tipping the head up or down. It's rarely serious unless it increases the risk of falling.\
+    People can experience dizziness, a spinning sensation (vertigo), lightheadedness, unsteadiness, loss of balance and nausea.\
+    Treatment includes a series of head movements that shift particles in the ears.",
+    "Acne":"Acne is a skin condition that occurs when your hair follicles become plugged with oil and dead skin cells. It causes whiteheads, blackheads or pimples. Acne is most common among teenagers, though it affects people of all ages.",
+    "Urinary tract infection":"Urinary tract infections are more common in women. They usually occur in the bladder or urethra, but more serious infections involve the kidney.\
+    A bladder infection may cause pelvic pain, increased urge to urinate, pain with urination and blood in the urine. A kidney infection may cause back pain, nausea, vomiting and fever.\
+    Common treatment is with antibiotics.",
+    "Psoriasis":"Psoriasis is thought to be an immune system problem. Triggers include infections, stress and cold.\
+    The most common symptom is a rash on the skin, but sometimes the rash involves the nails or joints.\
+    Treatment aims to remove scales and stop skin cells from growing so quickly. Topical ointments, light therapy and medication can offer relief.",
+    "Impetigo":"Impetigo mainly affects infants and children.\
+    The main symptom is red sores that form around the nose and mouth. The sores rupture, ooze for a few days, then form a yellow-brown crust.\
+    Antibiotics shorten the infection and can help prevent spread to others.",
+}
+
+
+
+
+
--- src/screens/main.js
+++ src/screens/main.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {Mark} from "../assets/human.js";
+import Logo from "../assets/logo.png";
 import Button from 'rsuite/Button';
 import { useEffect, useState } from "react";
-
+import { Container, Header, Content, Footer, Sidebar } from 'rsuite';
+import Navbar from 'rsuite/Navbar';
 import "../css/main.css";
 export const Main = ()=>{
     const [Symp,setSymp] = useState([]);
@@ -10,12 +12,17 @@ export const Main = ()=>{
         console.log("clicked"+Symp);
     }
     return(
-        <div className="container">
-            <div className="inner">
-               <Mark symp1 = {setSymp} symp2 = {Symp}/>
-               
-            </div>
-        </div>
+       <Container>
+        <Header>
+          <Navbar appearance="inverse">
+            <Navbar.Header>
+            <img src= {Logo} width="50" />
+            </Navbar.Header>
+          </Navbar>
+        </Header>
+        <Content className="inner">  <Mark symp1 = {setSymp} symp2 = {Symp}/></Content>
+        
+      </Container>
     )
 }
 
